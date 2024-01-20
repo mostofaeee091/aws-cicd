8a93218d5f02f1f352ea8ef0d0f32eef8713b1e8
Mostofa Kamal <mostofaeee091@gmail.com>
2024-01-21 00:02:00 +0600

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo systemctl start nginx
+# start_server.sh
+
+# Navigate to the Laravel project directory
+cd /var/www/html/laravel
+
+# Start the Laravel application server (adjust the command based on your setup)
+php artisan serve --host=0.0.0.0 --port=8000 > /dev/null 2>&1 &
