224d507773cba7bdafdf0258ac55e202fba009cf
Mostofa Kamal <mostofaeee091@gmail.com>
2024-01-20 23:03:15 +0600

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,8 +3,8 @@
 
 # Example: Install dependencies using Composer
 cd /var/www/html/laravel
-chmod +x scripts/install_dependencies.sh
-chmod +x scripts/configure_laravel.sh
-chmod +x scripts/start_server.sh
-chmod +x scripts/stop_server.sh
+sudo chmod +x scripts/install_dependencies.sh
+sudo chmod +x scripts/configure_laravel.sh
+sudo chmod +x scripts/start_server.sh
+sudo chmod +x scripts/stop_server.sh
 composer install --no-interaction --optimize-autoloader
