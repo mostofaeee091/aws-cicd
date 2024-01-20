d7fbb1c2f2cad4da61d7336befd8dde3518d5055
Mostofa Kamal <mostofaeee091@gmail.com>
2024-01-21 01:13:14 +0600

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # start_server.sh
-
+sudo apt install php
 # Navigate to the Laravel project directory
 cd /var/www/html/laravel
+
+# Install Composer dependencies
+composer install
+composer update
+
+# Cache configuration
+php artisan config:cache
 
 # Start the Laravel application server (adjust the command based on your setup)
 php artisan serve --host=0.0.0.0 --port=8000 > /dev/null 2>&1 &
